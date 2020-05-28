58859a02644ae2b6a2b286a4c224a2baebca64bc
im <81195314@qq.com>
2020-05-28 22:45:27 +0800

    update

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 //缓存空间名称
 var CACHE_VERSION = 'sw_v1'
 //需缓存的文件
-var CACHE_FILES = [ 'index.html']
+var CACHE_FILES = [ '/md/index.html', '/md/markdown-deck.min.js']
 
 //监听安装事件
 self.addEventListener('install', function (event) {
